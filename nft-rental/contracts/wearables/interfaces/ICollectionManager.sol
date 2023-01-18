1f659109ca90758ce848c63098a5e8da7edae28c
CWei88 <chenwei5lv@gmail.com>
2023-01-18 20:46:30 +0800

    Added decentraland stuff

---
+++ nft-rental/contracts/wearables/interfaces/ICollectionManager.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+
+interface ICollectionManager {
+   function manageCollection(address _forwarder, address _collection, bytes calldata _data) external;
+}
