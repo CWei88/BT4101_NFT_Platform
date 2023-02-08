c4c6c02e69567b929094ba6b54862f91a8143528
CWei88 <chenwei5lv@gmail.com>
2023-02-08 16:59:16 +0800

    Added Wrapping and UnWrapping test for ERC721 Tokens

--- rental-contracts/scripts/rent-dnft.js
+++ rental-contracts/scripts/rent-dnft.js
@@ -3,14 +3,15 @@ const {ethers} = require('hardhat')
 
 async function rent() {
     console.log("Getting token for rent")
-    const contractAddress = "0xaD09F27B6646f90AB4827A7dC22C5F975869050c"
-    const wToken = await ethers.getContractAt("Wrapped DNFT", contractAddress)
+    const contractAddress = "0x1476377940d5C8fC1524F6f5C7580585fDFcfF52"
+    const wrapperContract= require('../artifacts/contracts/ERC4907/ERC4907.sol/ERC4907.json')
+    const wToken = await ethers.getContractAt(wrapperContract.abi, contractAddress)
     const owner = '0xdC3A74E97F3D40Ebd0Ec64b9b01128b6E200969C'
 
     const renter = '0xD172885233efaa6Ce7018c0718D12550a2991196'
     const tokenId = '1'
     const expiry = Math.round(new Date().getTime() / 1000) + 600
-    let tx = await wToken.rent(tokenId, renter, expiry)
+    let tx = await wToken.setUser(tokenId, renter, expiry)
     await tx.wait();
 
     console.log(`NFT ${tokenId} rented to ${renter} until ${expiry}`)
--- rental-contracts/scripts/wrapper-dnft.js
+++ rental-contracts/scripts/wrapper-dnft.js
@@ -3,7 +3,7 @@ const {ethers} = require('hardhat')
 
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
 
-async function wrap() {
+async function deployWrapper() {
     const wrapper = await ethers.getContractFactory("ERC4907Wrapper")
     const wToken = await wrapper.deploy('0xaD09F27B6646f90AB4827A7dC22C5F975869050c', "Wrapped DNFT", "WDNFT")
 
@@ -11,11 +11,24 @@ async function wrap() {
 
     console.log(wToken.address)
     console.log("Wrapper deployed");
+}
+
+async function wrap() {
 
     let tokenId = '1'
-    let provider = ethers.getDefaultProvider('goerli')
-    let signer = new ethers.providers.JsonRpcProvider('0xdC3A74E97F3D40Ebd0Ec64b9b01128b6E200969C')
-    const wrap = await wToken.connect(signer).wrapToken('0');
+    const signer = await ethers.getSigner(PUBLIC_KEY);
+    console.log("Got Signer")
+
+    let contractAddress = '0x1476377940d5C8fC1524F6f5C7580585fDFcfF52'
+    //let a = provider.getSigner('0xdC3A74E97F3D40Ebd0Ec64b9b01128b6E200969C')
+    const wrapperContract= require('../artifacts/contracts/ERC4907/ERC4907Wrapper.sol/ERC4907Wrapper.json')
+    const wrapTest = await ethers.getContractAt(wrapperContract.abi, contractAddress)
+    const token= require('../artifacts/contracts/DiffTypeNFT.sol/DiffTypeNFT.json')
+    const tokenAddress = '0xaD09F27B6646f90AB4827A7dC22C5F975869050c'
+    const tokenTest = await ethers.getContractAt(token.abi, tokenAddress);
+    await tokenTest.approve(contractAddress, '1')
+    console.log('Contract Approved')
+    const wrap = await wrapTest.connect(signer).wrapToken('1');
     const wTx = await wrap.wait();
 
     let newAddress = wTx.events[2].address.toString();
@@ -25,7 +38,7 @@ async function wrap() {
 
 }
 
-wrap().then(() => process.exit(0))
+wrap().then()
 .catch((err) => {
     console.log(err)
     process.exit(1)
