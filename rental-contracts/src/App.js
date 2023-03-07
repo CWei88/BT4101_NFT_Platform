e057721fa004d6d4e86671be2e8dbb2bef3fdec3
junhanyang96 <71657066+junhanyang96@users.noreply.github.com>
2023-03-07 16:12:17 +0800

    Update App.js

--- rental-contracts/src/App.js
+++ rental-contracts/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import WalletCard from './WalletCard';
+import { useState } from 'react';
 
-function App() {
+//for routing
+import Layout from './pages/Layout.js';
+import Home from './pages/index.js';
+import Listings from './pages/Listings.js';
+import {Login} from './pages/Login.js';
+import Rent from './pages/Rent.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+  
+const App = () => {
+  const [data, setData] = useState('');
+  const childToParent = (childdata) => {
+    setData(childdata);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <WalletCard childToParent={childToParent}/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="listings" element={<Listings />} />
+              <Route path="login" element={<Login />} />
+              <Route path="Rent" element={<Rent />} />
+            </Route>
+          </Routes>
+      </BrowserRouter>
+      </div>
   );
 }
 
 export default App;
+
