da4fcbc067654fbd0ed0606b40c74891b84c483e
CWei88 <chenwei5lv@gmail.com>
2023-03-19 12:33:00 +0800

    Added scalability for Wrapper function

--- rental-contracts/contracts/ERC4907/ERC4907Wrapper.sol
+++ rental-contracts/contracts/ERC4907/ERC4907Wrapper.sol
@@ -2,52 +2,68 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./ERC4907.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract ERC4907Wrapper is ERC4907, IERC721Receiver {
+contract ERC4907Wrapper is ERC4907, IERC721Receiver, ReentrancyGuard {
 
-    address internal _tokenAddress;
-    address internal originalOwner;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address internal wrapperOwner;
+    bool internal wrappingAllowed;
 
     event TokenWrapped(address nftAddress, uint256 tokenId);
-
     event TokenUnWrapped(address nftAddress, uint256 tokenId);
+
+    struct OriginalToken {
+        address nftAddress;
+        uint256 tokenId;
+        address originalOwner;
+    }
+
+    mapping(uint256 => OriginalToken) private wrappedTokens;
 
     bytes4 public constant InterfaceIERC4907 = type(IERC4907).interfaceId;
 
-    constructor(address nftAddress, string memory _name, string memory _symbol) ERC4907(_name, _symbol) {
-        _tokenAddress = nftAddress;
+    modifier wrappingResumed() {
+        require(wrappingAllowed == true, "Wrapping has not been resumed");
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol) ERC4907(_name, _symbol) {
+        wrapperOwner = msg.sender;
+        wrappingAllowed = true;
     }
 
     function onERC721Received(address operator, address from, uint tokenId, bytes calldata data) public virtual override returns(bytes4)  {
         return this.onERC721Received.selector;
     }
 
-    function getTokenAddress() public view returns (address) {
-        return _tokenAddress;
+    function wrapToken(address _nftAddress, uint256 tokenId) public virtual wrappingResumed nonReentrant {
+        address owner = IERC721(_nftAddress).ownerOf(tokenId);
+        require(owner == msg.sender, "Only owner can wrap token");
+        uint256 currTokenId = _tokenIds.current();
+
+        ERC721(_nftAddress).safeTransferFrom(msg.sender, address(this), tokenId);
+        _safeMint(msg.sender, currTokenId);
+        wrappedTokens[currTokenId] = OriginalToken(_nftAddress, tokenId, msg.sender);
+        _tokenIds.increment();
+
+        emit TokenWrapped(_nftAddress, currTokenId);
     }
 
-    function wrapToken(uint256 tokenId) public virtual{
-        address owner = IERC721(_tokenAddress).ownerOf(tokenId);
-        require(owner == msg.sender, "Only owner can call this");
-        originalOwner = owner;
-
-        ERC721(_tokenAddress).safeTransferFrom(msg.sender, address(this), tokenId);
-        _safeMint(msg.sender, tokenId);
-
-        emit TokenWrapped(_tokenAddress, tokenId);
-    }
-
-    function unwrapToken(uint256 tokenId) public virtual{
-        address owner = IERC721(_tokenAddress).ownerOf(tokenId);
-        require(_isApprovedOrOwner(msg.sender, tokenId), "Only owner can unwrap");
-        require(owner == address(this), "No wrapped tokenId found");
+    function unwrapToken(uint256 tokenId) public virtual nonReentrant{
+        OriginalToken storage token = wrappedTokens[tokenId];
+        address currAddress = token.nftAddress;
+        require(IERC721(currAddress).ownerOf(token.tokenId) == address(this), "Invalid wrapped tokenId provided.");
+        require(token.originalOwner == msg.sender, "Only original owner can unwrap");
 
         _burn(tokenId);
-        ERC721(_tokenAddress).safeTransferFrom(address(this), _msgSender(), tokenId);
+        ERC721(token.nftAddress).safeTransferFrom(address(this), msg.sender, tokenId);
 
-        emit TokenUnWrapped(_tokenAddress, tokenId);
+        emit TokenUnWrapped(token.nftAddress, tokenId);
     }
 
     function _burn(uint256 tokenId) internal virtual override(ERC721) {
