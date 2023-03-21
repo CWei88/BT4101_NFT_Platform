4d75932c9f46e7c2ed34af56a67f72421aea54ab
CWei88 <chenwei5lv@gmail.com>
2023-03-21 23:45:21 +0800

    Remove Old Implementation

--- nft-rental/contracts/wearables/interfaces/ICollectionManager.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.17;
-
-
-interface ICollectionManager {
-   function manageCollection(address _forwarder, address _collection, bytes calldata _data) external;
-}
