b75fd79e9d2010b99466e404b62b303ae8dddcab
junhanyang96 <71657066+junhanyang96@users.noreply.github.com>
2023-04-02 19:00:05 +0800

    Add files via upload

---
+++ rental-contracts/src/components/Main.js
@@ -0,0 +1,103 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import Markdown from './Markdown';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import database from '../firebase.js'
+import Button from '@mui/material/Button';
+import { collection, query, where, getDocs } from "firebase/firestore";
+
+function Main(props) {
+  const { posts, title } = props;
+
+  const [postsList,setPosts] = React.useState([]);
+
+  async function getPosts() {
+    var posts = []
+    const q = query(collection(database, "Posts"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      var data = JSON.parse(JSON.stringify(doc.data()));
+      data['datetime'] = new Date(data['datetime']);
+      posts.push(data);
+    });
+    console.log(posts)
+    posts.sort(function(a,b){
+      if (a['datetime']>b['datetime']){
+        return -1;
+      } else {
+        return 1;
+      }
+    })
+    setPosts(posts)
+  }
+
+  return (
+    <Grid
+      item
+      xs={12}
+      md={8}
+      sx={{
+        '& .markdown': {
+          py: 3,
+        },
+      }}
+    >
+      <Grid container sx={{display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
+        <Typography variant="h6" gutterBottom>
+          {title}
+        </Typography>
+        <Button variant = 'outlined' onClick={()=>getPosts()}>Refresh Posts</Button>
+      </Grid>
+      <p></p>
+      <Divider />
+      {postsList.map((post) => (
+        <div>
+          
+          <Box sx={{ mt: 2, typography: 'body1',flexDirection:'column' }}>
+
+            <Grid container sx={{display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
+              <Typography variant='h6' fontWeight={'bold'}>
+                {post['title']}
+              </Typography>
+            </Grid>
+            <p></p>
+            <p></p>
+
+            <Typography variant='body1' fontSize={18}>{post['content']}</Typography>
+            <CardMedia
+                component="img"
+                sx={{
+                    width:'20%',
+                    pt: 0,
+                    margin:2
+                }}
+                image={post['image']}
+                alt="random"
+                />
+            <Grid container sx={{display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
+              <Typography variant='body2'>
+                Posted by: {post['author']}
+              </Typography>
+              <p></p>
+              <Typography variant='body1' fontStyle={'italic'}>
+                  {post['datetime'].toLocaleString()}
+              </Typography>
+            </Grid>
+          </Box>
+          <Divider/>
+        </div>
+      ))}
+    </Grid>
+  );
+}
+
+Main.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+export default Main;
---
+++ rental-contracts/src/pages/Blogs.js
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import MainFeaturedPost from '../components/MainFeaturedPost';
+import Main from '../components/Main';
+import Sidebar from '../components/Sidebar';
+import PostField from '../components/PostField.js'
+
+
+const mainFeaturedPost = {
+  title: 'Just got my hands on the newest release of #Seizon series!',
+  content:
+    "I am so excited! I have always loved this quirky bobblehead design!",
+  image: 'https://i.seadn.io/gcs/files/0b3ee4b7684fd76c62f75d5079e94734.gif?auto=format&w=750',
+};
+
+
+const theme = createTheme();
+
+export default function Blog() {
+
+  const sidebar = {
+    title: 'About FashRent & NFT Rarity',
+    description:
+      'Each precious NFT comes with its unique traits. We want owners and everyone to appreciate the value of every token. Do not hesitate to show off your NFTs by posting them! You can also see whose NFTs are the rariest on average!',
+    social: [
+      { name: 'GitHub', icon: GitHubIcon },
+      { name: 'Twitter', icon: TwitterIcon },
+      { name: 'Facebook', icon: FacebookIcon },
+    ],
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <main>
+          <PostField></PostField>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          <Grid container spacing={5} sx={{ mt: 3 }}>
+            <Main title="Newest Posts"/>
+            <Sidebar
+              title={sidebar.title}
+              description={sidebar.description}
+              social={sidebar.social}
+            />
+          </Grid>
+        </main>
+      </Container>
+    </ThemeProvider>
+  );
+}
